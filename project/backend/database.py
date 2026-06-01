25a4e8421bbdf51feffe3a7a6c47f59750e2378a
Alicja Przeździecka <ala.przezdziecka@gmail.com>
2026-06-01 14:15:31 +0200

    docker dev/test/prod

--- project/backend/database.py
+++ project/backend/database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/namesdb"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@localhost:5432/namesdb"
+)
 
 engine = create_engine(DATABASE_URL)
 
